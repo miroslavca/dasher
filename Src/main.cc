c2ccf1cac68909a02df2beda443cf23c2a2ee3e6
Mursel Caglar <murselca@gmail.com>
2017-03-28 13:57:18 -0700

    Added some comment line for translators. issue #40

--- Src/main.cc
+++ Src/main.cc
@@ -175,7 +175,10 @@ int main(int argc, char *argv[]) {
   GError *error = nullptr;
   if (!g_option_context_parse(goptcontext, &argc, &argv, &error)) {
     g_print("option parsing failed: %s\n", error->message);
-    g_print("Try 'dasher --help' for more information.\n");
+    /* Note to translators: This is the help string for unrecognized command line options.
+     * Do not translate 'dasher --help' part. 
+     */
+    g_print("%s\n", _("Try 'dasher --help' for more information.")); 
     exit (1);
   }
 
